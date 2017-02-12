5fa8c525d7ebb360ffa98a3f1eca1c5610bf3863
cesar rodas <rodascesar03@yahoo.com>
2017-02-12 16:15:04 -0600

    added password encrypting with bcrypt

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -4,14 +4,27 @@ exports.signup = function(req, res, next) {
   const email = req.body.email;
   const password = req.body.password;
   // see if user with the email exists
+
+  if (!email || !password) {
+    return res.status(422).send({ error: 'You must provide email and password.'})
+  }
   User.findOne({email: email }, function(err, existingUser) {
     if (err) { return next(err); }
 
+    // if a user with email does exist, return an Error
     if(existingUser) {
       return res.status(422).send({ error: 'Email is in use' });
     }
   });
-  // if a user with email does exist, return an Error
   // if a user with email does NOT exist, create and save user record
+  const user = new User({
+    email: email,
+    password: password
+  });
+
+  user.save(function(err) {
+    if (err) { return next(err); }
+    res.json({ success: true });
+  });
   // respond to request indicating the user was created
 }
